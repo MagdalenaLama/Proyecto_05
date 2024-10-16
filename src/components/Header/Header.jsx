9e4272a7b01e046122f34204e5507291d21d5f79
Magdalena Lama <magdalenalamalyon@gmail.com>
2024-10-16 14:16:06 -0300

    feat: Home icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,22 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { IoHome } from "react-icons/io5";
+
 export const Header = ({ title }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const showHomeIcon = location.pathname === "/search";
+  const handleClickIcon = () => {
+    navigate("/");
+  };
   return (
     <>
-      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-700">
+      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-700 flex items-center">
+        {showHomeIcon && (
+          <IoHome
+            className="text-blue-500 text-4xl cursor-pointer"
+            onClick={handleClickIcon}
+          />
+        )}
         <div className="max-w-screen-xl flex items-center justify-center mx-auto p-4">
           <h1 className="text-center text-xl font-bold text-gray-900 dark:text-white">
             {title}
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,29 +7,39 @@ export const Modal = ({ closeModal, title, handleOutsideClick }) => {
         className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
         onClick={handleOutsideClick}
       >
-        <div className="bg-white rounded-lg shadow-lg p-6 max-w-lg w-full">
-          <button
-            className="text-gray-500 float-right font-bold"
-            onClick={closeModal}
-          >
-            &times;
-          </button>
-          <img
-            className="object-cover h-48 w-full mb-4"
-            src={
-              title.volumeInfo.imageLinks?.thumbnail
-                ? title.volumeInfo.imageLinks.thumbnail
-                : imgNF
-            }
-            alt={title.volumeInfo.title}
-          />
-          <h5 className="text-2xl font-bold mb-2">{title.volumeInfo.title}</h5>
-          <h6 className="text-lg font-semibold mb-2">
-            Autor: {title.volumeInfo.authors || "Desconocido"}
-          </h6>
-          <p className="text-gray-700">
-            {title.volumeInfo.description || "Descripción no disponible"}
-          </p>
+        <div className="bg-white rounded-lg shadow-lg p-6 container max-w-3xl">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="max-h-96">
+              <img
+                className="object-cover h-full w-full mb-4"
+                src={
+                  title.volumeInfo.imageLinks?.thumbnail
+                    ? title.volumeInfo.imageLinks.thumbnail
+                    : imgNF
+                }
+                alt={title.volumeInfo.title}
+              />
+            </div>
+            <div>
+              <button
+                className="text-gray-500 float-right font-bold"
+                onClick={closeModal}
+              >
+                &times;
+              </button>
+
+              <h5 className="text-2xl font-bold mb-2">
+                {title.volumeInfo.title}
+              </h5>
+              <h6 className="text-lg font-semibold mb-2">
+                Autor: {title.volumeInfo.authors || "Desconocido"}
+              </h6>
+              {/* Contenedor de texto desplazable */}
+              <p className="text-gray-700 max-h-48 overflow-y-auto">
+                {title.volumeInfo.description || "Descripción no disponible"}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
